af993167e1aa52d494d909201bf149b07b939777
Sufian <sufian.babri@gmail.com>
2020-12-13 20:38:12 +0500

    Converted AnimationRevealPage to TypeScript.

--- src/helpers/AnimationRevealPage.tsx
+++ src/helpers/AnimationRevealPage.tsx
@@ -4,18 +4,26 @@ import tw from 'twin.macro';
 /* framer-motion and useInView here are used to animate the sections in when we reach them in the viewport
  */
 import {motion} from 'framer-motion';
+// @ts-ignore
 import useInView from 'use-in-view';
 
 const StyledDiv = tw.div`font-display min-h-screen text-secondary-500 p-8 overflow-hidden`;
 
-function AnimationReveal({disabled, children}) {
+type DirectionType = 'left' | 'right' | 'top' | 'bottom';
+
+interface IRevealProps {
+	disabled?: boolean;
+	children: JSX.Element[];
+}
+
+function AnimationReveal({disabled, children}: IRevealProps) {
 	if (disabled) {
 		return <>{children}</>;
 	}
 
 	if (!Array.isArray(children)) children = [children];
 
-	const directions = ['left', 'right'];
+	const directions: DirectionType[] = ['left', 'right'];
 	const childrenWithAnimation = children.map((child, i) => {
 		return (
 			<AnimatedSlideInComponent key={i} direction={directions[i % directions.length]}>
@@ -26,13 +34,19 @@ function AnimationReveal({disabled, children}) {
 	return <>{childrenWithAnimation}</>;
 }
 
-function AnimatedSlideInComponent({direction = 'left', offset = 30, children}) {
+interface IComponentProps {
+	direction: DirectionType;
+	offset?: number;
+	children: JSX.Element;
+}
+
+function AnimatedSlideInComponent({direction = 'left', offset = 30, children}: IComponentProps) {
 	const [ref, inView] = useInView(30);
 
-	const x = {target: '0%'};
-
-	if (direction === 'left') x.initial = '-150%';
-	else x.initial = '150%';
+	const x = {
+		target: '0%',
+		initial: direction === 'left' ? '-150%' : '150%'
+	};
 
 	return (
 		<motion.section
@@ -51,7 +65,11 @@ function AnimatedSlideInComponent({direction = 'left', offset = 30, children}) {
 	);
 }
 
-export default props => (
+interface IProps {
+	children: JSX.Element[];
+}
+
+export default (props: IProps) => (
 	<StyledDiv className="App">
 		<AnimationReveal {...props} />
 	</StyledDiv>
